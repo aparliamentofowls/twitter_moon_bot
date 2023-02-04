8ab0338beeb579ca4038008c7122e88054c8d156
owl <fannywang162@gmail.com>
2023-02-03 16:28:34 -0800

    Writes method for api call to Weather Api (in order to obtain moon phase & moonrise data)

--- moonrise_moonset.py
+++ moonrise_moonset.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.common.exceptions import TimeoutException
@@ -8,12 +9,17 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
 
-# Constants
+# Constants - Selenium Related
 CHROME_DRIVER_PATH = os.environ["CDPATH"]
 TWITTER_URL = "https://twitter.com/i/flow/login"
 TWITTER_PASSWORD = os.environ["TWPASSWORD"]
 TWITTER_EMAIL = os.environ['TWEMAIL']
 TWITTER_USERNAME = os.environ['TWUSERNAME']
+
+# Constants - Weather Api Related
+VANCOUVER_LAT_LONG = "49.246292,-123.116226"
+WEATHER_API_KEY = os.environ['API_KEY']
+WEATHER_API_URL = 'http://api.weatherapi.com/v1/astronomy.json'
 
 
 class MoonriseMoonsetBot:
@@ -37,8 +43,20 @@ class MoonriseMoonsetBot:
         password_textfield.send_keys(TWITTER_PASSWORD)
         password_textfield.send_keys(Keys.RETURN)
 
+    def retrieve_moon_info(self):
+        params = {
+            "key": WEATHER_API_KEY,
+            "q": VANCOUVER_LAT_LONG,
+        }
+        moon_data = requests.get(url=WEATHER_API_URL, params=params).json()
+        moon_phase = moon_data['astronomy']['astro']['moon_phase']
+        print(moon_phase)
+        moonrise = moon_data['astronomy']['astro']['moonrise']
+        print(moonrise)
+
 
 myMoonBot = MoonriseMoonsetBot()
+myMoonBot.retrieve_moon_info()
 myMoonBot.sign_in_to_twitter()
 time.sleep(10)
 myMoonBot.driver.close()
